75e2d46800a1eb336db6df434ca827ceaa77b332
w.endo <w.endo@E0403-50>
2022-12-06 15:32:19 +0900

    プロジェクト ファイルを追加します。

---
+++ Engine/Fbx.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <d3d11.h>
+#include <fbxsdk.h>
+#include <string>
+#include "Transform.h"
+
+
+#pragma comment(lib, "LibFbxSDK-MT.lib")
+#pragma comment(lib, "LibXml2-MT.lib")
+#pragma comment(lib, "zlib-MT.lib")
+
+class Texture;
+
+
+class Fbx
+{
+	//マテリアル
+	struct MATERIAL
+	{
+		Texture* pTexture;
+		XMFLOAT4	diffuse;
+	};
+
+	struct CONSTANT_BUFFER
+	{
+		XMMATRIX matWVP;
+		XMMATRIX matNormal;
+		XMMATRIX matW;
+		XMFLOAT4 color;
+		XMFLOAT4 camPos;
+		int		 isTexture;
+	};
+
+	struct VERTEX
+	{
+		XMVECTOR position;
+		XMVECTOR uv;
+		XMVECTOR normal;
+	};
+	int vertexCount_;	//頂点数
+	int polygonCount_;	//ポリゴン数
+	int materialCount_;	//マテリアルの個数
+
+	ID3D11Buffer* pVertexBuffer_;
+	ID3D11Buffer** pIndexBuffer_;
+	ID3D11Buffer* pConstantBuffer_;
+	MATERIAL* pMaterialList_;
+
+	int* indexCount_;
+
+public:
+	Fbx();
+	~Fbx();
+	HRESULT Load(std::string fileName);
+	void    Draw(Transform& transform);
+	void    Release();
+
+private:
+	void InitVertex(fbxsdk::FbxMesh* pMesh);
+	void InitIndex(fbxsdk::FbxMesh* pMesh);
+	void IntConstantBuffer();
+	void InitMaterial(fbxsdk::FbxNode* pNode);
+};
---
+++ Test.hlsl
@@ -0,0 +1,72 @@
+Texture2D tex : register(t0);
+SamplerState smp : register(s0);
+
+cbuffer gloabl
+{
+	float4x4 matWVP;
+	float4x4 matNormal;
+	float4x4 matW;
+	float4	 color;
+	float4	 camPos;
+	bool	 isTexture;
+};
+
+struct VS_OUT
+{
+	float4 pos : SV_POSITION;
+	float2 uv  : TEXCOORD;
+	float4 normal : NORMAL;
+	float4 V : TEXCOORD1;
+};
+
+//頂点シェーダー
+VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
+{
+	VS_OUT outData;
+	outData.pos = mul(pos, matWVP);
+	outData.uv = uv;
+
+
+	normal.w = 0;
+	outData.normal = mul(normal, matNormal);
+	outData.normal = normalize(outData.normal);
+
+	outData.V = normalize(mul(pos, matW) - camPos);
+
+	return outData;
+}
+
+//ピクセルシェーダー
+float4 PS(VS_OUT inData) : SV_TARGET
+{
+	float4 diffuse;
+	float4 ambient;
+	float4 specular;
+
+	float4 light = float4(1, 1, -1, 0);
+	light = normalize(light);
+
+	float4 S = dot(inData.normal, light);
+	S = clamp(S, 0, 1);
+//	if (S.r < 0.3)
+//		S = 0.3;
+//	else
+//		S = 1;
+
+
+	float4 R = reflect(light, inData.normal);
+	specular = pow(clamp(dot(R, inData.V), 0, 1), 10) * 3;
+
+	if (isTexture)
+	{
+		diffuse = tex.Sample(smp, inData.uv) * S;
+		ambient = tex.Sample(smp, inData.uv) * 0.2;
+	}
+	else
+	{
+		diffuse = color * S;
+		ambient = color * 0.2;
+	}
+
+	return diffuse + ambient + specular;
+}
